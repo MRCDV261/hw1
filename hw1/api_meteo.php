45293e11e26617ce43833c9065c72835027dc0fa
Marco Di Vona <marco.divona13@gmail.com>
2025-05-27 15:10:44 +0200

    Update api_meteo.php

--- hw1/api_meteo.php
+++ hw1/api_meteo.php
@@ -3,7 +3,7 @@
     $lat = $_GET['lat'];
     $lng = $_GET['lng'];
 
-    $apiKey = '7c15ad8a5a0e46a8b62110903251204';
+    $apiKey = 'secret';
     $url = "https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$lat,$lng&lang=it";
 
     $curl = curl_init();
